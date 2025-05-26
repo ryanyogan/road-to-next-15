27c8c21e27dc579da944b29c52431817026cb742
Ryan Yogan <ryanyogan@gmail.com>
2025-05-25 20:08:17 -0500

    tickets feature start

---
+++ src/features/ticket/components/ticket-item.tsx
@@ -0,0 +1,65 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { cn } from "@/lib/utils";
+import { ticketPath } from "@/paths";
+import { LucideArrowUpRightFromSquare } from "lucide-react";
+import Link from "next/link";
+import { TICKET_ICONS } from "../constants";
+import { Ticket } from "../types";
+
+type TicketItemProps = {
+  ticket: Ticket;
+  isDetail?: boolean;
+};
+
+export function TicketItem({ ticket, isDetail }: TicketItemProps) {
+  const detailButton = (
+    <Button variant="outline" size="icon" asChild>
+      <Link href={ticketPath(ticket.id)}>
+        <LucideArrowUpRightFromSquare className="size-4" />
+      </Link>
+    </Button>
+  );
+
+  return (
+    <div
+      className={cn("w-full flex gap-x-1", {
+        "max-w-[580px]": isDetail,
+        "max-w-[420px]": !isDetail,
+      })}
+    >
+      <Card className="w-full">
+        <CardHeader>
+          <CardTitle className="flex gap-x-2">
+            <span>{TICKET_ICONS[ticket.status]}</span>
+            <span className="truncate">{ticket.title}</span>
+          </CardTitle>
+        </CardHeader>
+        <CardContent>
+          <span
+            className={cn("whitespace-break-spaces", {
+              "line-clamp-3": !isDetail,
+            })}
+          >
+            {ticket.content}
+          </span>
+        </CardContent>
+        <CardFooter>
+          <Link href={ticketPath(ticket.id)} className="text-sm underline">
+            View
+          </Link>
+        </CardFooter>
+      </Card>
+
+      {isDetail ? null : (
+        <div className="flex flex-col gap-y-1">{detailButton}</div>
+      )}
+    </div>
+  );
+}
